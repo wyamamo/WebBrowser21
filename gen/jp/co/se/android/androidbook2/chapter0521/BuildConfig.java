623dfc7ad5125aa2dc2f5e19b953899a4f601ebf
wyamamo <vatarushka@gmail.com>
2015-08-03 18:19:09 +0900

    Remove ignore files

--- gen/jp/co/se/android/androidbook2/chapter0521/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package jp.co.se.android.androidbook2.chapter0521;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
